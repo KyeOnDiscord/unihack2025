4a1c6f13cd603d4a231f710e094dc68aa9a3bbb3
Jason Yi <jasonyi1413@gmail.com>
2025-03-15 02:33:15 +1100

    Added API auth

--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 # See https://docs.python.org/3/faq/programming.html#how-do-i-share-global-variables-across-modules
 
+import os
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -7,6 +10,8 @@ if TYPE_CHECKING:
 
     from models.config_models import AppConfigDto
 
+
+INTERFACE_API_KEY: str = os.getenv("INTERFACE_API_KEY")
 
 app: FastAPI = None
 app_config: AppConfigDto = None
