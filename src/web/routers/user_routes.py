e3295bd19f95c0202fe2c4d39ce79e8511191841
Tansimran <tansimranssandhu@outlook.com>
2025-03-15 18:49:40 +1100

    fixed fixed verify email

--- src/web/routers/user_routes.py
+++ src/web/routers/user_routes.py
@@ -125,6 +125,6 @@ async def verify_user(token:str):
             user_collection = await config.db.get_collection(CollectionRef.USERS)
 
             user.account_verified = True
-            await user_collection.update_one({UserRef.ACCOUNT_VERIFIED: user.account_verified}, {"$set": user.model_dump_safe()})
+            await user_collection.update_one({UserRef.ID: user.id},{"$set": {UserRef.ACCOUNT_VERIFIED: True}})
 
             return {"message": "User email has been successfully verified"}
