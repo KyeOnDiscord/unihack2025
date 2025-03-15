15dbd92ca421096828d882da5747492f89d6bf79
Tansimran <tansimranssandhu@outlook.com>
2025-03-15 14:45:16 +1100

    room model

---
+++ src/models/room_models.py
@@ -0,0 +1,10 @@
+# Chuck in anything related to the room model here.
+# e.g. The rooms, users they hold, and id
+
+from .generic import DBRecord
+from typing import Optional, List
+
+class RoomDto(DBRecord):
+    id: Optional[str] = None
+    name: str
+    users: List[str] = []
